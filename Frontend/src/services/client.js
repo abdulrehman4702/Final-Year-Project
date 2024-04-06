034b1d8875fff162439487cb3a7fd640a7dd1a4b
Abdul Rehman <129248657+abdulrehman4702@users.noreply.github.com>
2024-04-06 20:55:02 +0500

    Client.JS Code
    I comment the All code in Client.JS code

--- Frontend/src/services/client.js
+++ Frontend/src/services/client.js
@@ -1,25 +1,25 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const loginCustomer=async(customer)=>{
-    try {
-        return await axios.post(`http://localhost:8000/api/user/login`,customer)
-    } catch (error) {
-        throw error;
-    }
-}
+// export const loginCustomer=async(customer)=>{
+//     try {
+//         return await axios.post(`http://localhost:8000/api/user/login`,customer)
+//     } catch (error) {
+//         throw error;
+//     }
+// }
 
-export const registerCustomer=async(customer)=>{
-    try {
-        return await axios.post(`http://localhost:8000/api/user/register`,customer)
-    } catch (error) {
-        throw error;
-    }
-}
+// export const registerCustomer=async(customer)=>{
+//     try {
+//         return await axios.post(`http://localhost:8000/api/user/register`,customer)
+//     } catch (error) {
+//         throw error;
+//     }
+// }
 
-export const changeUserPassword=async(customer)=>{
-    try {
-        return await axios.post(`http://localhost:8000/api/user/changepassword`,customer)
-    } catch (error) {
-        throw error;
-    }
-}
+// export const changeUserPassword=async(customer)=>{
+//     try {
+//         return await axios.post(`http://localhost:8000/api/user/changepassword`,customer)
+//     } catch (error) {
+//         throw error;
+//     }
+// }
